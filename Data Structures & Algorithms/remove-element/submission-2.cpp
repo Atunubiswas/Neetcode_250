e21b336e88164096866599b80f8f694646532698
AtunuBiswas <112483046+Atunubiswas@users.noreply.github.com>
2026-09-02 14:09:30 +0530

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    /*
+        Time complexity: O(n + n) = O(2n)
+        Space complexity: O(1)
+    */
+
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+
+        int firstInvalidIndex = -1;
+        for(int i = 0; i <= n - 1; i++) {
+            if(nums[i] == val) {
+                firstInvalidIndex = i;
+                break;
+            }
+        }
+
+        if(firstInvalidIndex != -1) {
+            for(int i = firstInvalidIndex + 1; i <= n - 1; i++) {
+                if(nums[i] != val) {
+                    swap(nums[i], nums[firstInvalidIndex++]);
+                }
+            }
+        }
+
+        return firstInvalidIndex == -1 ? n : firstInvalidIndex;
+    }
+};  
