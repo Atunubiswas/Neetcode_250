dd09f079d186fb1331ec988f300b55fd5ea747f9
AtunuBiswas <112483046+Atunubiswas@users.noreply.github.com>
2026-09-05 20:43:52 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    /*
+        Using STL library 
+            Time complexity: O(nlogn)
+            Space complexityL O(1)
+    */
+
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+    }
+};
