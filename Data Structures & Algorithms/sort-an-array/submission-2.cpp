57e37a219bb266ab136894f237a5865c1f852f76
AtunuBiswas <112483046+Atunubiswas@users.noreply.github.com>
2026-09-05 20:42:26 +0530

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    /*
+        Time complexity: O(n^2)
+        Space complexity: O(1)
+    */
+
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+
+        for(int i = 0; i <= n - 1; i++) {
+            int minNumber = nums[i], minIndex = i;
+            for(int j = i; j <= n - 1; j++) {
+                if(nums[j] < minNumber) {
+                    minNumber = min(minNumber, nums[j]);
+                    minIndex = j;
+                }
+            }
+            if(minIndex != i) {
+                swap(nums[i], nums[minIndex]);
+            }
+        }
+
+        return nums;
+    }
+};
