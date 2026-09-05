38fdd4e30c9328a1d85ac60ce303fe00b1765ab5
AtunuBiswas <112483046+Atunubiswas@users.noreply.github.com>
2026-09-05 21:20:21 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    /*
+        Time complexity: O(2n + nlogn + k)
+        Space complexity: O(2n + k)
+    */
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        unordered_map<int, int> countMap;
+        for(int i = 0; i <= n - 1; i++) {
+            countMap[nums[i]]++;
+        }
+
+        vector<pair<int, int> > storage;
+        for(auto it: countMap) {
+            storage.push_back({it.second, it.first});
+        }
+
+        sort(storage.rbegin(), storage.rend());
+
+        vector<int> ans;
+        for(int i = 0; i < k; i++) {
+            ans.push_back(storage[i].second);
+        }
+
+        return ans;
+    }
+};
